6129a8322ae4178921aac42e289b17f42e4d929b
Rehan Butt <rab@Rehans-MacBook-Pro-2.local>
2017-05-12 00:15:16 -0400

    Next button fixed

---
+++ public/js/app2.js
@@ -0,0 +1,15 @@
+$(document).foundation()
+
+//var input = document.getElementById('text-field').autofocus;
+
+$(function() {
+  $("text-field").focus();
+});
+
+$(document).keydown(function(e){
+  if (e.keyCode == 13) {
+    window.location.replace("/relation")
+  }
+});
+
+$("#guess").focus();
